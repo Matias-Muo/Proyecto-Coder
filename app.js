ab5848d2576a595fa1703da5f68ac5d31b51a586
Matias Muo <matiasmuo@gmail.com>
2022-12-11 20:12:26 -0300

    ifakunle

---
+++ app.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'BIENVENIDO A LA CASA DE IFÁ',
+    text: 'INGRESAR',
+    imageUrl: './img/ifakunle.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
